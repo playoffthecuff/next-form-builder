cebabc1df30de7d9e3700dd572abea4da33a0aaa
playoffthecuff <jmobile@bk.ru>
2025-03-21 19:26:39 +0300

    feat: add header

---
+++ src/components/header/children/logo.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function Logo() {
+  return (
+    <Link href="/" className="font-medium text-2xl bg-gradient-to-r from-indigo-400 to-cyan-300 text-transparent bg-clip-text">PageForm</Link>
+  )
+}
