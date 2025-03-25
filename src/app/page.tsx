5728d8dfe2133ad295b4e005574b7537a450ec98
playoffthecuff <jmobile@bk.ru>
2025-03-25 12:01:22 +0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import FormCards from "@/components/cards/form/cards";
 import FormCardSkeleton from "@/components/cards/form/skeleton";
 import CardStatsWrapper from "@/components/cards/wrapper";
 import { Separator } from "@/components/ui/separator";
-import Image from "next/image";
 import { Suspense } from "react";
 
 export default function Home() {
@@ -26,53 +25,6 @@ export default function Home() {
 					</div>
 				</div>
 			</main>
-			<footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/file.svg"
-						alt="File icon"
-						width={16}
-						height={16}
-					/>
-					Learn
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/window.svg"
-						alt="Window icon"
-						width={16}
-						height={16}
-					/>
-					Examples
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/globe.svg"
-						alt="Globe icon"
-						width={16}
-						height={16}
-					/>
-					Go to nextjs.org →
-				</a>
-			</footer>
 		</>
 	);
 }
